038f03b718f1cc241586048d788ad44a74dfd434
Tomas <bekjon_98@mail.ru>
2024-05-02 20:38:44 +0300

    update

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -1,14 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, Image } from 'react-native'
+import { useState } from 'react';
+import { icons } from '../constants';
 import React from 'react'
 
-const FormField = () => {
+const FormField = ({title, value, placeholder, handleChangeText, otherStyles, ...rest}) => {
+
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <View>
-      <Text>FormField</Text>
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
+      <View className="border-2 w-full h-16 px-4 bg-black-100 border-black-200 rounded-2xl focus:border-secondary items-center flex-row">
+        <TextInput
+          className="flex-1 text-white font-psemibold text-base"
+          value={value}
+          placeholder={placeholder}
+          placeholderTextColor={"#7b7b8b"}
+          onChangeText={handleChangeText}
+          secureTextEntry={title === 'Password' && !showPassword}
+         />
+         {title === 'Password' && (
+          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+            <Image source={showPassword ? icons.eye : icons.eyeHide} className={'w-6 h-6 '} resizeMode='contain' />
+          </TouchableOpacity>
+         ) }
+      </View>
     </View>
   )
 }
 
-export default FormField
+export default FormField;
 
 const styles = StyleSheet.create({})
