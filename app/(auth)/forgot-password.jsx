efc6383bea502f0b7c1aef6e73d52901dec42da9
Tomas <bekjon_98@mail.ru>
2024-10-13 12:04:57 +0300

    update

--- app/(auth)/forgot-password.jsx
+++ app/(auth)/forgot-password.jsx
@@ -1,5 +1,5 @@
 import { View, Text, ScrollView, Image, TouchableOpacity, TextInput } from 'react-native'
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect, useCallback } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import {  router } from 'expo-router';
 import { GoogleSigninButton } from '@react-native-google-signin/google-signin';
@@ -21,12 +21,18 @@ const ForgotPassword = () => {
     setCurStage(prev => prev + 1);
   }
 
+  const getStageName = () => {
+    if (curStage === 1) return "Forgot Password"
+    if (curStage === 2) return "Verification"
+    return "New Password";
+  }
+
   return (
     <SafeAreaView className="bg-[#F7F7F7] h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
         <View className='w-full flex-1 justify-between'>
           <View className="w-full h-full justify-between max-h-[60%] pt-[23px] items-start">
-            <Text className="font-inter text-[24px] text-[#000000] font-bold px-[25px] pt-10 pb-9">{curStage !== 1 ? "Verification" : "Forgot Password"}</Text>
+            <Text className="font-inter text-[24px] text-[#000000] font-bold px-[25px] pt-10 pb-9">{getStageName()}</Text>
             <View className="w-full h-full justify-start px-[31px] max-h-[320px]">
               <View className="w-full flex-row">
                 <Stage active={curStage === 1} />
@@ -54,6 +60,8 @@ const ForgotPassword = () => {
                 </View>
               ) : curStage === 2 ? (
                 <SentTo email={email} setEmail={setEmail} />
+              ) : curStage === 3 ? (
+                <NewPassowrd /> 
               ) : <></> }
             </View>
           </View>
@@ -72,8 +80,6 @@ const Stage = ({ active = false, styles }) => {
 const SentTo = ({ email, setEmail }) => {
 
   const inputRef = useRef(null);
-
-  const { formattedTime, seconds, minutes } = useTimer(0, 25);
 
   const handleEditEmail = (e) => {
     setEmail(e)
@@ -102,27 +108,141 @@ const SentTo = ({ email, setEmail }) => {
       </TouchableOpacity>
     </View>
     <View className="mt-[50px] items-center">
-      <Text className="text-[20px] leading-[20px] tracking-[.1px] text-[#373737] font-robotoblack">Enter Verification Code</Text>
-      <Text className="text-[15px] leading-[20px] tracking-[.1px] text-[#262626] font-robotoblack font-bold text-center mt-[12px]">{formattedTime}</Text>
-      <View className="w-full mt-[24px] flex-row">
-        <DigitSell />
-        <DigitSell />
-        <DigitSell />
-        <DigitSell />
-      </View>
-      { seconds === 0 && minutes === 0 && (
-        <TouchableOpacity className="border border-[#000000] rounded-[100px] mt-[47px] w-[327px] h-[40px] items-center justify-center">
-          <Text className="text-sm leading-[20px] tracking-[.1px] font-medium font-roboto">Resend Code</Text>
-        </TouchableOpacity>
-      ) }
+      <TimeLeft />
+      <DigitSellInput />
+      <ResendButton />
     </View>
   </View>
 }
 
 
-const DigitSell = ({ value }) => {
-   return <View className="border border-[#000000] items-center justify-center mr-2 rounded-[12px] w-[50px] h-[57px]">
-    <TextInput maxLength={1} value={value} className='text-[18px] leading-[20px] tracking-[.1px] font-roboto text-center' />
+const TimeLeft = () => {
+  const { formattedTime } = useTimer(0, 25);
+
+  return <Text className="text-[15px] leading-[20px] tracking-[.1px] text-[#262626] font-robotoblack font-bold text-center mt-[12px]">{formattedTime}</Text>
+}
+
+
+const ResendButton = () => {
+
+  const { seconds, minutes } = useTimer(0, 25);
+
+  return <>
+      { seconds === 0 && minutes === 0 && (
+        <TouchableOpacity className="border border-[#000000] rounded-[100px] mt-[47px] w-[327px] h-[40px] items-center justify-center">
+          <Text className="text-sm leading-[20px] tracking-[.1px] font-medium font-roboto">Resend Code</Text>
+        </TouchableOpacity>
+      ) }
+  </>
+}
+
+
+
+
+const DigitSell = ({ value, setValue }) => {
+
+  return (
+    <View className="border border-[#000000] items-center justify-center mr-2 rounded-[12px] w-[50px] h-[57px]">
+      <TextInput
+        maxLength={1}
+        value={value}
+        //onChangeText={(e) => setValue(prev => ([...prev, e]))}
+        className='text-[18px] leading-[20px] tracking-[.1px] font-roboto text-center'
+      />
+    </View>
+  );
+}
+
+const DigitSellInput = () => {
+
+  const [verificationCode, setVerificationCode] = useState([]);
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [])
+
+
+  const callback = useCallback(() => {
+    if (verificationCode.length === 4) {
+      //make api call
+      alert("the passsword is sent")
+    }
+  }, [verificationCode.length])
+
+  callback();
+
+  console.log(verificationCode)
+
+  return  (
+      <View className="w-full mt-[24px] flex-row">
+        <TextInput
+          className="opacity-0"
+          keyboardType="numeric"
+          ref={inputRef}
+          maxLength={4}
+          onChangeText={(e) => setVerificationCode(prev => [...prev, e]) }
+        />
+        <DigitSell
+          value={verificationCode.length > 0 ? verificationCode[0][0] : null}
+          //setValue={setVerificationCode}
+        />
+        <DigitSell
+          value={verificationCode.length > 1 ? verificationCode[1][1] : null}
+          //setValue={setVerificationCode}
+        />
+        <DigitSell
+          value={verificationCode.length > 2 ? verificationCode[2][2] : null} 
+          //setValue={setVerificationCode}
+        />
+        <DigitSell
+          value={verificationCode.length > 3 ? verificationCode[3][3] : null}
+          //setValue={setVerificationCode}
+        />
+      </View>
+      )
+}
+
+const NewPassowrd = () => {
+
+  const [password, setNewPassword] = useState({
+    oldPassword: "",
+    newPassword: ""
+  });
+
+  const handleUpdate = () => {}
+
+  return <View className="w-full ">
+    <FormField
+      title={"Enter New Password"}
+      titleStyles={"font-roboto text-[#373737] font-medium text-[14px] leading-[20px] tracking-[.1px]"}
+      placeholder={'random@domain.com'}
+      placeholderTextColor={"#777777"}
+      textInputStyles={'bg-[#EFEFEF] text-[12px] leading-[20px] tracking-[.1px] font-roboto font-semibold'}
+      textInputContainerStyles={'bg-[#EFEFEF] rounded-[5px] border-0'}
+      otherStyles={'max-h-[52px] h-full mb-[50px]'}
+      handleChangeText={(e) => setNewPassword(prev => ({ ...prev, oldPassword: e })) }
+      value={password.oldPassword}
+    />
+
+    <FormField
+      title={"Confirm New Password"}
+      titleStyles={"font-roboto text-[#373737] font-medium text-[14px] leading-[20px] tracking-[.1px]"}
+      placeholder={'random@domain.com'}
+      placeholderTextColor={"#777777"}
+      textInputStyles={'bg-[#EFEFEF] text-[12px] leading-[20px] tracking-[.1px] font-roboto font-semibold'}
+      textInputContainerStyles={'bg-[#EFEFEF] rounded-[5px] border-0'}
+      otherStyles={'max-h-[52px] h-full mb-[50px]'}
+      handleChangeText={(e) => setNewPassword(prev => ({ ...prev, newPassword: e })) }
+      value={password.newPassword}
+    />
+
+    <TouchableOpacity
+      onPress={handleUpdate}
+      className="rounded-[100px] bg-[#373737] max-w-[327px] w-full h-[40px] items-center justify-center">
+      <Text className="text-sm font-roboto leading-[20px] tracking-[.1px] text-white text-center">Update</Text>
+    </TouchableOpacity>
   </View>
 }
 
