ff967974b6d187afe549f5b45f12e8ecc5dfb623
Tomas <bekjon_98@mail.ru>
2024-10-22 18:38:40 +0300

    update welcome screen

--- app/index.jsx
+++ app/index.jsx
@@ -11,12 +11,15 @@ import { PrimaryButton, SignInWithProvider } from '../components/CustomButton';
   webClientId: '691662689785-pngqudprjbmp2hpl4navnjdull1hrndv.apps.googleusercontent.com'
 }); */
 
+// there's certain problem with welcome screen on galaxy s models
+// sign in link is not visible
+
 export default function App() {
 
     return (
         <SafeAreaView className="bg-[#F7F7F7] h-full">
             <ScrollView contentContainerStyle={{ height: '100%' }}>
-                <View className="py-[40px] w-full">
+                <View className="py-[30px] w-full">
                     <Text className="font-cygrebold color-[#000000] pb-2 text-center text-[24px]">Welcome To Bookish Beasts !</Text>
                     <View className="font-cygreregular space-y-0 leading-[20px] text-center px-[50px] w-full"> 
 
@@ -32,20 +35,20 @@ export default function App() {
                 <View className="items-center">
                     <Image source={images.welcome} resizeMode='contain' className='w-[185px] h-[202px]' />
                 </View>
-                <View className='items-center mb-[28px] mt-[33px]'>
+                <View className='items-center mb-[28px] mt-[33px] px-[20px]'>
                     <PrimaryButton title={"Sign up"}
                         handlePress={() => router.push('(auth)/sign-up')}
                         containerStyles={'max-w-[353px] w-full rounded-3xl'}
                         textStyles={'text-center justify-center items-center'}
                     />
                 </View>
-                <View className='items-center'>
+                <View className='items-center px-[20px]'>
                     <SignInWithProvider title={'Continue with Google'} containerStyles={'max-w-[353px] w-full rounded-3xl'} textStyles={'text-center ml-3'} />
                     <SignInWithProvider provider='facebook' title={'Continue with Facebook'} containerStyles={'max-w-[353px] my-[10px] w-full rounded-3xl'} textStyles={'text-center ml-3'} />
                     <SignInWithProvider provider='apple' title={'Continue with Apple'} containerStyles={'max-w-[353px] w-full rounded-3xl'} textStyles={'text-center ml-3'} />
                 </View>
-                <TouchableOpacity onPress={() => router.push('(auth)/sign-in')} className="mb-[20px]">
-                    <Text className='font-cygreregular text-[#373737] text-[16px] pt-[15px] text-center'>Sign In</Text>
+                <TouchableOpacity onPress={() => router.push('(auth)/sign-in')} className="flex-1 h-full">
+                    <Text className='font-cygreregular text-[#373737] text-[16px] pt-[28px] text-center'>Sign In</Text>
                 </TouchableOpacity>
             </ScrollView>
             <StatusBar backgroundColor='#F7F7F7' style='dark' />
