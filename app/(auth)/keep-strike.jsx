7a96c0f03b064316766e383ce7b97d6f1ff2c1be
Tomas <bekjon_98@mail.ru>
2024-10-27 11:34:36 +0200

    update onboarding keep-strike screen

--- app/(auth)/keep-strike.jsx
+++ app/(auth)/keep-strike.jsx
@@ -9,11 +9,10 @@ import { images } from "../../constants";
 const KeepStrike = () => {
 
     return <SafeAreaView className="bg-[#F7F7F7] h-full">
-            <ScrollView contentContainerStyle={{ height: "100%" }}>
             <View className="w-full px-[20px] mt-[20px]">
                 <OnboardingProgress stage1={16} />
             </View>
-            <View className="w-full mt-[77px] mb-[5px] items-center">
+            <View className="w-full mt-12 mb-[5px] items-center flex-[.1]">
                 <Text className="font-bold max-w-[181px] text-[24px] mb-[20px] text-center leading-[23px] font-cygrebold">
                     Keep the strike !
                 </Text>
@@ -21,7 +20,7 @@ const KeepStrike = () => {
                     And get a new Bookish Beast to your collection.
                 </Text>
             </View>
-            <View className="space-x-1 flex-row items-start flex-1">
+            <View className="space-x-1 flex-row justify-center items-start mt-2 flex-[1]">
                 <View className="gap-[16px] relative top-8">
                     <View className="relative">
                         <Image source={images.monster1} width={139} height={166} />
@@ -32,7 +31,7 @@ const KeepStrike = () => {
                         <Image source={images.question} width={45} height={77} className="absolute top-14 left-6" />
                     </View>
                 </View>
-                <View className="gap-[16px] self-end mb-4">
+                <View className="gap-[16px] self-center mb-2">
                     <View className="relative">
                         <Image source={images.monster3} width={139} height={166} />
                         <Image source={images.question} width={45} height={77} className="absolute top-14 left-12" />
@@ -53,12 +52,13 @@ const KeepStrike = () => {
                     </View>
                 </View>
             </View>
-            <TouchableOpacity
-                onPress={() => router.push('/set-strike')}
-                className="bg-[#6592E3] max-w-[313px] w-full self-center mb-[30px] items-center justify-center max-h-[52px] h-full rounded-[47px]">
-                <Text className="text-[#FEFEFC] text-[18px] leading-[22px] font-semibold">Continue</Text>
-            </TouchableOpacity>
-        </ScrollView>
+            <View className="w-full flex-[.2]">
+                <TouchableOpacity
+                    onPress={() => router.push('/set-strike')}
+                    className="bg-[#6592E3] max-w-[313px] w-full self-center mb-[30px] items-center justify-center max-h-[52px] h-full rounded-[47px]">
+                    <Text className="text-[#FEFEFC] text-[18px] leading-[22px] font-semibold">Continue</Text>
+                </TouchableOpacity>
+            </View>
         <StatusBar backgroundColor='#F7F7F7' style='dark' />
         </SafeAreaView>
 }
