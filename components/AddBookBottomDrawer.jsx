e53d40996a00daeac2496ead46515b79858d928f
Tomas <bekjon_98@mail.ru>
2024-11-08 21:43:47 +0200

    drawer's backdrop animation appears immediately fix

--- components/AddBookBottomDrawer.jsx
+++ components/AddBookBottomDrawer.jsx
@@ -6,8 +6,9 @@ import {
     Dimensions,
     TouchableOpacity,
     Pressable,
+    Animated
 } from "react-native";
-
+import { useRef, useEffect } from "react";
 import { MaterialIcons } from '@expo/vector-icons';
 import { router } from "expo-router";
 
@@ -15,6 +16,9 @@ const AddBookBottomDrawer = ({ isBottomSheetOpen, setIsBottomSheetOpen }) => {
 
     const windowHeight = Dimensions.get('window').height;
 
+    const slideAnim = useRef(new Animated.Value(0)).current;
+
+    const liftDistance = windowHeight * 0.0001;
 
     // Function to open the bottom sheet 
     const handleOpenBottomSheet = () => {
@@ -26,17 +30,37 @@ const AddBookBottomDrawer = ({ isBottomSheetOpen, setIsBottomSheetOpen }) => {
         setIsBottomSheetOpen(false);
     };
 
+    useEffect(() => {
+        if (isBottomSheetOpen) {
+        slideAnim.setValue(0);
+        Animated.timing(slideAnim, {
+            toValue: 1,
+            duration: 300,
+            useNativeDriver: true,
+        }).start();
+        }
+    }, [isBottomSheetOpen]);
+
+    const translateY = slideAnim.interpolate({
+        inputRange: [0, 1],
+        outputRange: [windowHeight, -liftDistance],
+    });
+
 
     return <Modal
-            animationType="slide"
+            animationType="none"
             transparent={true}
             visible={isBottomSheetOpen}
             onRequestClose={handleOpenBottomSheet}>
                 <Pressable
                     onPress={handleCloseBottomSheet}
-                    className="absolute h-full w-full z-0 bg-[#3D3D3D61] opacity-[38]"
-                ></Pressable>
-                <View className="rounded-t-[30px] p-4 z-10" style={[styles.bottomSheet, { height: windowHeight * 0.3 }]}>
+                    className="absolute transition-opacity h-full w-full z-0 bg-[#3D3D3D61] opacity-[38]">
+                </Pressable>
+                <Animated.View
+                    className="rounded-t-[30px] p-4 z-10"
+                    style={[styles.bottomSheet, { height: '30%', transform: [{ translateY }],
+                    }]}>
+
                     <Text className="text-[#000000] mb-6 font-cygrebold text-[22px] leading-[26.4px]">Add a Book</Text>
                     <TouchableOpacity
                         onPress={() => {
@@ -52,7 +76,7 @@ const AddBookBottomDrawer = ({ isBottomSheetOpen, setIsBottomSheetOpen }) => {
                         <MaterialIcons name='keyboard' size={25} color={'#fff'} />
                         <Text className="text-[#FEFEFC] font-cygrebold leading-[19.2px] ml-6">Add Manually</Text>
                     </TouchableOpacity>
-                </View>
+                </Animated.View>
         </Modal>
 }
 
