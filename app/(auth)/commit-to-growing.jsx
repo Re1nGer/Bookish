7fc41a953b362f65d81b32f0e42bea1f93cf5113
Tomas <bekjon_98@mail.ru>
2024-12-01 19:32:43 +0200

    updated DaylyTiming component, fixed commit-to-growing screen: made selectable options

--- app/(auth)/commit-to-growing.jsx
+++ app/(auth)/commit-to-growing.jsx
@@ -4,10 +4,25 @@ import { router } from "expo-router";
 import { StatusBar } from 'expo-status-bar';
 import OnboardingProgress from "../../components/OnboardingProgress";
 import DailyTiming from "../../components/DailyTiming";
+import { useState } from "react";
 
 
 
 const CommitToGrowing = () => {
+
+    const [days, setDays] = useState({
+        7: false,
+        14: false,
+        30: false,
+        50: false,
+    });
+
+    const defaultDays = {
+        7: false,
+        14: false,
+        30: false,
+        50: false,
+    };
 
     return <SafeAreaView className="bg-[#F7F7F7] h-full">
         <ScrollView contentContainerStyle={{ height: "100%" }}>
@@ -20,14 +35,37 @@ const CommitToGrowing = () => {
                 </Text>
             </View>
             <View className="items-center flex-1 mx-[23px]">
-                <DailyTiming times={"7-day strike"} level="Promising" styles={"mb-[14px]"} />
-                <DailyTiming times={"14-day strike"} level="Determined" styles={"mb-[14px]"} />
-                <DailyTiming times={"30-day strike"} level="Impressive" styles={"mb-[14px]"} />
-                <DailyTiming times={"50-day strike"} level="Unstoppable" />
+                <DailyTiming
+                    times={"7-day strike"}
+                    level="Promising"
+                    styles={"mb-[14px]"}
+                    selected={days[7]}
+                    onPress={() => setDays((_) => ({...defaultDays, 7: true}))}
+                />
+                <DailyTiming
+                    times={"14-day strike"}
+                    level="Determined"
+                    styles={"mb-[14px]"}
+                    selected={days[14]} 
+                    onPress={() => setDays((_) => ({...defaultDays, 14: true}))}
+                />
+                <DailyTiming
+                    times={"30-day strike"}
+                    level="Impressive"
+                    styles={"mb-[14px]"}
+                    selected={days[30]} 
+                    onPress={() => setDays((_) => ({...defaultDays, 30: true}))}
+                />
+                <DailyTiming
+                    times={"50-day strike"}
+                    level="Unstoppable"
+                    selected={days[50]} 
+                    onPress={() => setDays((_) => ({...defaultDays, 50: true}))}
+                />
             </View>
             <TouchableOpacity
                 onPress={() => router.push('/keep-strike')}
-                className="bg-[#6592E3] max-w-[313px] w-full self-center mb-[30px] items-center justify-center max-h-[52px] h-full rounded-[47px]">
+                className="bg-primary max-w-[313px] w-full self-center mb-[30px] items-center justify-center max-h-[52px] h-full rounded-[47px]">
                 <Text className="text-[#FEFEFC] text-[18px] leading-[22px] font-semibold">Continue</Text>
             </TouchableOpacity>
         </ScrollView>
--- components/DailyTiming.jsx
+++ components/DailyTiming.jsx
@@ -2,10 +2,11 @@ import { View, Text, TouchableOpacity } from "react-native";
 
 
 
-const DailyTiming = ({ times, level, styles, selected }) => {
+const DailyTiming = ({ onPress, times, level, styles, selected }) => {
 
     return <TouchableOpacity
-        className={`max-w-[348px] w-full bg-[#FFFFFF] border-[.9px] rounded-[15px] border-[#8A8A8A] justify-center max-h-[60px] h-full ${styles}`}>
+        onPress={onPress}
+        className={`max-w-[348px] w-full bg-[#FFFFFF] border-[.9px] rounded-[15px] border-[#8A8A8A] justify-center max-h-[60px] h-full ${selected ? 'border-primary border-[2px]' : ''} ${styles}`}>
         <View className="flex-row justify-around">
             <View className="ml-[27px] justify-center">
                 <Text className="text-[#000000] text-[16px] leading-[19.2px] font-bold">{times}</Text>
