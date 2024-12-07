7b8200ad2f9b5c56f6937471c4d646870d60eb5b
Tomas <bekjon_98@mail.ru>
2024-12-07 22:37:18 +0200

    fixed create-note scroll view layout issue

--- app/(auth)/create-note.jsx
+++ app/(auth)/create-note.jsx
@@ -79,7 +79,7 @@ const CreateNote = () => {
         }
     };
 
-    return <SafeAreaView className="bg-[#F7F7F7] h-full">
+    return <SafeAreaView style={{ flex: 1 }} className="bg-[#F7F7F7] h-full max-h-full">
             <View className="max-h-[60px] justify-between items-center flex-row h-full mx-5 mb-7">
                 <View className="flex-row items-center mt-2">
                     <TouchableOpacity
@@ -95,109 +95,112 @@ const CreateNote = () => {
                 </TouchableOpacity>
             </View>
 
-        <ScrollView contentInsetAdjustmentBehavior="automatic" className="h-full">
-            <Pressable
-                onPress={() => inputRef.current?.focus()}
-                className="mt-5 mx-5 relative max-h-[317px] h-full border-[#8A8A8A] rounded-[20px] border-[.5px] py-3 px-4">
-                    <View className="flex-row">
-                        <View className="bg-[#F8846A] max-w-[95px] mt-4 mr-2 max-h-[25px] w-full h-full justify-center items-center rounded-[13px]">
-                            <Text className="text-sm text-white font-cygre semibold leading-[16.8px] text-center">Fact</Text>
+            <ScrollView>
+
+                <TouchableOpacity
+                    onPress={() => inputRef.current?.focus()}
+                    className="mt-5 mx-5 relative h-[317px] border-[#8A8A8A] rounded-[20px] border-[.5px] py-3 px-4">
+                        <View className="flex-row">
+                            <View className="bg-[#F8846A] max-w-[95px] mt-4 mr-2 max-h-[25px] w-full h-full justify-center items-center rounded-[13px]">
+                                <Text className="text-sm text-white font-cygre semibold leading-[16.8px] text-center">Fact</Text>
+                            </View>
+                            <View className="bg-[#E6E6E6] max-w-[95px] mt-4 max-h-[25px] w-full h-full justify-center items-center rounded-[13px]">
+                                <Text className="text-sm text-white font-cygre semibold leading-[16.8px] text-center">{new Date().toLocaleDateString('de-DE')}</Text>
+                            </View>
                         </View>
-                        <View className="bg-[#E6E6E6] max-w-[95px] mt-4 max-h-[25px] w-full h-full justify-center items-center rounded-[13px]">
-                            <Text className="text-sm text-white font-cygre semibold leading-[16.8px] text-center">{new Date().toLocaleDateString('de-DE')}</Text>
-                        </View>
+                    <TextInput
+                        value={text}
+                        placeholder="Enter your note here"
+                        ref={inputRef}
+                        onChangeText={(e) => setText(e)}
+                        onBlur={() => setIsFocused(false)}
+                        onFocus={() => setIsFocused(true)}
+                        multiline
+                        className="py-4 w-full max-h-[317px]"
+                        onSelectionChange={handleSelectionChange}
+                        selection={selection}
+                    />
+                </TouchableOpacity>
+    {/*             { isFocused && (
+                    <ScrollView
+                        horizontal
+                        showsHorizontalScrollIndicator={false}
+                        contentInsetAdjustmentBehavior="automatic"
+                        className="mx-5 my-5 absolute bottom-0"
+                    >
+                        <FormatButton 
+                            icon="format-bold" 
+                            onPress={handleBoldPress}
+                            label="Bold"
+                            containerStyles={'mr-2.5'}
+                        />
+                        <FormatButton 
+                            icon="format-italic" 
+                            label="Italic"
+                            containerStyles={'mr-2.5'}
+                        />
+                        <FormatButton 
+                            icon="format-list-bulleted" 
+                            label="Bullet list"
+                            containerStyles={'mr-2.5'}
+                        />
+                        <FormatButton 
+                            icon="format-list-numbered" 
+                            label="Numbered list"
+                            containerStyles={'mr-2.5'}
+                        />
+                        <FormatButton 
+                            icon="link" 
+                            label="Link"
+                            containerStyles={'mr-2.5'}
+                        />
+                        <FormatButton 
+                            icon="title" 
+                            label="Heading"
+                            containerStyles={'mr-2.5'}
+                        />
+                        <FormatButton 
+                            icon="format-quote" 
+                            label="Quote"
+                            containerStyles={'mr-2.5'}
+                        />
+                        <FormatButton 
+                            icon="title" 
+                            label="Heading"
+                            containerStyles={'mr-2.5'}
+                        />
+                        <FormatButton 
+                            icon="format-quote" 
+                            label="Quote"
+                            containerStyles={'mr-2.5'}
+                        />
+                    </ScrollView>
+                ) } */}
+                <View className="mt-9 mx-5 max-h-[160px]">
+                    <Text className="text-black text-[22px] leading-[26.4px] font-cygrebold mb-2.5">Spaced Repetition Groups</Text>
+                    <View className="flex-wrap p-5 border bg-black max-h-[126px] w-full h-full flex-row items-center rounded-[20px]">
+                        <Genre name={'Memory Improvement'} showCloseBtn={false} containerStyles={'max-w-[200px]'} />
                     </View>
-                <TextInput
-                    value={text}
-                    placeholder="Enter your note here"
-                    ref={inputRef}
-                    onChangeText={(e) => setText(e)}
-                    onBlur={() => setIsFocused(false)}
-                    onFocus={() => setIsFocused(true)}
-                    multiline
-                    className="py-4 w-full max-h-[317px]"
-                    onSelectionChange={handleSelectionChange}
-                    selection={selection}
-                />
-            </Pressable>
-    {/*         { isFocused && (
-                <ScrollView
-                    horizontal
-                    showsHorizontalScrollIndicator={false}
-                    contentInsetAdjustmentBehavior="automatic"
-                    className="mx-5 my-5 absolute bottom-0"
-                >
-                    <FormatButton 
-                        icon="format-bold" 
-                        onPress={handleBoldPress}
-                        label="Bold"
-                        containerStyles={'mr-2.5'}
-                    />
-                    <FormatButton 
-                        icon="format-italic" 
-                        label="Italic"
-                        containerStyles={'mr-2.5'}
-                    />
-                    <FormatButton 
-                        icon="format-list-bulleted" 
-                        label="Bullet list"
-                        containerStyles={'mr-2.5'}
-                    />
-                    <FormatButton 
-                        icon="format-list-numbered" 
-                        label="Numbered list"
-                        containerStyles={'mr-2.5'}
-                    />
-                    <FormatButton 
-                        icon="link" 
-                        label="Link"
-                        containerStyles={'mr-2.5'}
-                    />
-                    <FormatButton 
-                        icon="title" 
-                        label="Heading"
-                        containerStyles={'mr-2.5'}
-                    />
-                    <FormatButton 
-                        icon="format-quote" 
-                        label="Quote"
-                        containerStyles={'mr-2.5'}
-                    />
-                    <FormatButton 
-                        icon="title" 
-                        label="Heading"
-                        containerStyles={'mr-2.5'}
-                    />
-                    <FormatButton 
-                        icon="format-quote" 
-                        label="Quote"
-                        containerStyles={'mr-2.5'}
-                    />
-                </ScrollView>
-            ) } */}
-            <View className="mt-9 mx-5 max-h-[160px] h-full">
-                <Text className="text-black text-[22px] leading-[26.4px] font-cygrebold mb-2.5">Spaced Repetition Groups</Text>
-                <View className="flex-wrap p-5 border bg-black max-h-[126px] w-full h-full flex-row items-center rounded-[20px]">
-                    <Genre name={'Memory Improvement'} showCloseBtn={false} containerStyles={'max-w-[200px]'} />
                 </View>
-            </View>
 
-            <View className="mt-5 mx-5 max-h-[160px] h-full">
-                <Text className="text-black text-[22px] leading-[26.4px] font-cygrebold mb-2.5">Collections</Text>
-                <View className="flex-wrap p-5 border bg-black max-h-[126px] h-full flex-row items-center rounded-[20px]">
-                    <Genre name={'Nonfiction'} showCloseBtn={false} />
-                    <Genre name={'Self Help'} showCloseBtn={false} />
-                    <Genre name={'Psychology'} showCloseBtn={false} />
+                <View className="mt-5 mx-5 max-h-[160px]">
+                    <Text className="text-black text-[22px] leading-[26.4px] font-cygrebold mb-2.5">Collections</Text>
+                    <View className="flex-wrap p-5 border bg-black max-h-[126px] h-full flex-row items-center rounded-[20px]">
+                        <Genre name={'For psychology classes'} showCloseBtn={false} containerStyles={'max-w-[200px]'} />
+                    </View>
                 </View>
-            </View>
 
-            <View className="mt-5 mx-5 bg-black max-h-[160px] h-full flex-row items-center rounded-[20px]">
-                <View className="mx-7">
-                    <Text className="font-cygrebold leading-[19.2px] font-bold text-[#fff] max-w-[157px]">Add quotes you liked from this book</Text>
+                <View className="mx-5 mt-5">
+                    <Text className="text-whtie text-[22px] leading-[26.4px] font-cygrebold">Quote</Text>
                 </View>
-                <QuoteStarsIcon />
-            </View>
-        </ScrollView>
+                <View className="my-2.5 mx-5 max-h-[106px] bg-black h-full flex-row items-center rounded-[20px]">
+                    <View className="mx-7">
+                        <Text className="font-cygrebold leading-[19.2px] font-bold text-[#fff] max-w-[157px]">Add quotes you liked from this book</Text>
+                    </View>
+                    <QuoteStarsIcon />
+                </View>
+            <View className="h-[50px]"></View>
+            </ScrollView>
     </SafeAreaView>
 }
 
