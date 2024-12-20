e41e9f14225dfce817502fcb58a23e267e386b77
Tomas <bekjon_98@mail.ru>
2024-12-20 18:50:10 +0200

    fix border right spacing, cards rendered between in android

--- app/(auth)/keep-strike.jsx
+++ app/(auth)/keep-strike.jsx
@@ -13,14 +13,14 @@ const KeepStrike = () => {
                 <OnboardingProgress stage1={16} />
             </View>
             <View className="w-full mt-12 mb-[5px] items-center flex-[.1]">
-                <Text className="font-bold max-w-[181px] text-[24px] mb-[20px] text-center leading-[27px] font-cygrebold">
+                <Text className="font-bold text-[24px] mb-[20px] text-center leading-[27px] font-cygrebold">
                     Keep the strike !
                 </Text>
                 <Text className="leading-[19.2px] text-center font-cygreregular max-w-[269px] w-full px-8">
                     And get a new Bookish Beast to your collection.
                 </Text>
             </View>
-            <View className="flex-row items-start mt-2 flex-1">
+            <View className="flex-row items-start justify-between mt-2 flex-1">
                 <View className="gap-[16px] relative top-8 mr-4">
                     <View className="relative">
                         <Image source={images.monster1} width={139} height={166} />
@@ -41,7 +41,7 @@ const KeepStrike = () => {
                         <Image source={images.question} width={45} height={77} className="absolute top-14 left-12" />
                     </View>
                 </View>
-                <View className="gap-[16px]">
+                <View className="space-y-[16px]">
                     <View className="relative">
                         <Image source={images.monster5} width={139} height={166} />
                         <Image source={images.question} width={45} height={77} className="absolute top-14 left-12" />
