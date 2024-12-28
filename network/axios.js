a149f2e181a655e960f2218dfe47406cd8766558
Tomas <bekjon_98@mail.ru>
2024-12-28 18:21:46 +0200

    updated axios base url

--- network/axios.js
+++ network/axios.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import { Platform } from 'react-native';
 
 
 const instance = axios.create({
-  baseURL: "http://164.92.195.88/", //dev env
+  baseURL: Platform.OS === 'android' ? "http://10.0.2.2:5085/" : 'http://localhost:5085/', //dev env
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
