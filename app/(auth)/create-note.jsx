a0c5fca7376795b7025aa48021a247bae6c1ce71
Tomas <bekjon_98@mail.ru>
2025-01-01 17:10:17 +0200

    refactored create-note screen into separate components: CreateNoteTypeDrawer, DefaultNoteType, NoteType, NoteTypeDrawer

--- app/(auth)/create-note.jsx
+++ app/(auth)/create-note.jsx
@@ -5,9 +5,6 @@ import {
     TextInput,
     Pressable,
     ScrollView,
-    KeyboardAvoidingView,
-    Platform,
-    FlatList
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { MaterialIcons } from '@expo/vector-icons';
@@ -17,28 +14,16 @@ import Genre from "../../components/Genre";
 import { QuoteStarsIcon } from "../../components/Svg";
 import BookBottomDrawer from "../../components/BottomDrawer";
 import Fontisto from '@expo/vector-icons/Fontisto';
-import FormField from '../../components/FormField';
 import { UserContext } from "../../context/UserContext";
 import QuoteCard from "../../components/QuoteCard";
 import { Alert } from 'react-native';
 import axios from '../../network/axios';
+import DefaultNoteType from "../../components/DefaultNoteType";
+import NoteTypeDrawer from "../../components/NoteTypeDrawer";
+import CreateNoteTypeDrawer from "../../components/CreateNoteTypeDrawer";
 
 
 
-const defaultColors = {
-    black: false,
-    orange: false,
-    lightYellow: false,
-    brightYellow: false,
-    green: false,
-    blue: false,
-    violet: false,
-    pink: false,
-    turquoise: false,
-    red: false,
-    brown: false,
-    oliveGreen: false
-}
 
 
 const CreateNote = () => {
@@ -137,7 +122,7 @@ const CreateNote = () => {
 
     useEffect(() => {
         fetchNoteTypes();
-    }, [isNoteTypeDrawerOpen])
+    }, [isNoteTypeDrawerOpen]);
 
     useEffect(() => {
         inputRef.current?.focus();
@@ -149,7 +134,7 @@ const CreateNote = () => {
                 text: ''
             });
         }
-    }, [])
+    }, []);
 
 
     return <SafeAreaView className="bg-[#F7F7F7] h-full">
@@ -284,259 +269,4 @@ const CreateNote = () => {
 export default CreateNote;
 
 
-const DefaultNoteType = ({ onPress, bgColor, text }) => {
 
-   return (
-        <TouchableOpacity
-            onPress={onPress}
-            style={{ backgroundColor: bgColor }}
-            className="max-w-[95px] mt-4 mr-2 max-h-[25px] w-full h-full justify-center items-center rounded-[13px]">
-            <Text className="text-sm text-white font-cygrebold leading-[16.8px] text-center" numberOfLines={1} ellipsizeMode="tail">{text}</Text>
-        </TouchableOpacity>
-   );
-}
-
-const NoteTypeDrawer = ({ 
-    noteTypes,
-    isNoteDrawerOpen,
-    setIsNoteDrawerOpen,
-    setIsNoteTypeDrawerOpen,
-    setNoteTypesSelected,
-    noteTypesSelected
- }) => {
-
-
-    const handleChoseNoteType = (id) => {
-        //update single entry, the rest set to false
-        setNoteTypesSelected((prev) => {
-        const resetObj = Object.fromEntries(Object.keys(prev).map(key => [key, false]));
-            return {...resetObj, [id]: !prev[id]};
-        });
-    }
-
-    return (
-            <BookBottomDrawer
-                height="55%"
-                isBottomSheetOpen={isNoteDrawerOpen}
-                setIsBottomSheetOpen={setIsNoteDrawerOpen}
-                containerStyles={'pb-0 flex-1'}
-            >
-                    <Text className="font-cygrebold text-[22px] mt-9 leading-[26.4px] text-center mb-5">Choose Note’s Type</Text>
-                    <FlatList
-                        className="w-full mx-4 flex-1"
-                        data={noteTypes}
-                        ListEmptyComponent={() => <Text className="text-[20px] font-cygrebold">No Note Types Yet</Text>}
-                        renderItem={({ item }) =>
-                            <NoteType
-                                key={item.id}
-                                name={item.name}
-                                icon={item.icon}
-                                bgColor={item.bgColor}
-                                selected={noteTypesSelected[item.id]}
-                                onPress={() => handleChoseNoteType(item.id)}
-                            />}
-                    />
-                    <TouchableOpacity
-                        onPress={() => setIsNoteTypeDrawerOpen(true)}
-                        className="bg-black mb-2 justify-center rounded-[34px] max-h-[56px] items-center w-full h-full">
-                        <Text className="text-white font-cygrebold text-[18px] text-center">Add New Type</Text>
-                    </TouchableOpacity>
-            </BookBottomDrawer>
-        );
-}
-
-const NoteType = ({ name, icon, bgColor, selected, onPress }) => {
-    return (
-        <TouchableOpacity
-                onPress={onPress}
-                style={{backgroundColor: bgColor}}
-                className={`mt-2 flex-row justify-start pl-6 flex-1 mb-1 rounded-[15px] h-[56px] items-center ${selected ? 'border-black border' : ''}`}>
-            <Text className="text-[20px]">{icon}</Text>
-            <Text className="text-white pl-9 font-cygrebold text-[18px]">{name}</Text>
-        </TouchableOpacity>
-    );
-}
-
-const CreateNoteTypeDrawer = ({ isNoteTypeDrawerOpen, setIsNoteTypeDrawerOpen }) => {
-
-    const [name, setName] = useState('');
-    const [icon, setIcon] = useState('');
-    const [error, setError] = useState({ icon: '' });
-
-    const [colors, setColors] = useState({
-        black: true,
-        orange: false,
-        lightYellow: false,
-        brightYellow: false,
-        green: false,
-        blue: false,
-        violet: false,
-        pink: false,
-        turquoise: false,
-        red: false,
-        brown: false,
-        oliveGreen: false
-    });
-
-    const getColorHesh = (color) => {
-        switch(color) {
-            case 'black':
-                return '#000';
-            case 'orange':
-                return '#F8846A';
-            case 'lightYellow':
-                return '#FFCA57';
-            case 'brightYellow':
-                return '#FFF946';
-            case 'green':
-                return '#1BBA3B';
-            case 'blue':
-                return '#4D81E0';
-            case 'violet':
-                return '#633EE9';
-            case 'pink':
-                return '#F473C0';
-            case 'turquoise':
-               return '#7AD4DE';
-            case 'red':
-               return '#9D1414';
-            case 'brown':
-                return '#6F3416';
-            case 'oliveGreen':
-                return '#65875A';
-            default:
-                return '#fff';
-        }
-    }
-
-    const getColor = () => {
-        return Object.keys(colors).find(key => colors[key] === true);
-    }
-
-    const handleSaveNoteType = async () => {
-        console.log(getColorHesh(getColor()), name, icon)
-        try {
-            await axios.post('users/note/type', {
-                color: getColorHesh(getColor()),
-                name: name,
-                icon: icon
-            });
-            setIsNoteTypeDrawerOpen(false);
-        } catch(error) {
-            console.log(error);
-        }
-    }
-
-    const handleNameChange = (name) => {
-        setName(name);
-    }
-
-    const handleIconChange = (iconArg) => {
-        if (isLetter(iconArg.at(-1))) {
-            //emoji takes up 2 letter spaces, so we should should take into account for two letter case
-            setIconError('input should be an icon')
-        } 
-        else {
-            setIconError('')
-        }
-        setIcon(iconArg);
-    }
-
-    const getIconError = () => {
-        return error['icon']
-    }
-
-    const setIconError = (error) => {
-        setError(prev => ({...prev, icon: error}));
-    }
-
-    const isLetter = (char) => {
-        return /^\p{L}$/u.test(char);
-    };
-
-    const ColorButton = ({ color, borderColor, bgColor }) => (
-        <TouchableOpacity
-            onPress={() => setColors(() => ({...defaultColors, [color]: !colors[color]}))}
-            className={`border-[2px] rounded-[6px] max-w-[29px] w-full h-[28px]`}
-            style={{ backgroundColor: colors[color] ? bgColor : '', borderColor: borderColor }}
-        />
-    );
-
-    return (
-        <BookBottomDrawer
-                height="80%"
-                isBottomSheetOpen={isNoteTypeDrawerOpen}
-                setIsBottomSheetOpen={setIsNoteTypeDrawerOpen}
-                containerStyles={'pb-0'}
-            >
-            <KeyboardAvoidingView
-                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-                style={{ flex: 1 }}
-                keyboardVerticalOffset={Platform.OS === 'ios' ? 40 : 0}
-            >
-                <ScrollView 
-                    className="flex-1"
-                    contentInsetAdjustmentBehavior="automatic"
-                    keyboardShouldPersistTaps="handled"
-                    
-                >
-                    <View style={{ flex: 1 }} className="w-full">
-                        <Text className="font-cygrebold text-[22px] mt-9 leading-[26.4px] text-center">
-                            Create New Type
-                        </Text>
-
-                        <View className="my-6 rounded-[20px] items-center border-[.5px] py-4">
-                            <View className="flex-row mb-6 justify-evenly w-full">
-                                <ColorButton color="black" borderColor="#000" bgColor="#000" />
-                                <ColorButton color="orange" borderColor="#F8846A" bgColor="#F8846A" />
-                                <ColorButton color="lightYellow" borderColor="#FFCA57" bgColor="#FFCA57" />
-                                <ColorButton color="brightYellow" borderColor="#FFF946" bgColor="#FFF946" />
-                                <ColorButton color="green" borderColor="#1BBA3B" bgColor="#1BBA3B" />
-                                <ColorButton color="blue" borderColor="#4D81E0" bgColor="#4D81E0" />
-                            </View>
-                            <View className="flex-row justify-evenly w-full">
-                                <ColorButton color="violet" borderColor="#633EE9" bgColor="#633EE9" />
-                                <ColorButton color="pink" borderColor="#F473C0" bgColor="#F473C0" />
-                                <ColorButton color="turquoise" borderColor="#7AD4DE" bgColor="#7AD4DE" />
-                                <ColorButton color="red" borderColor="#9D1414" bgColor="#9D1414" />
-                                <ColorButton color="brown" borderColor="#6F3416" bgColor="#6F3416" />
-                                <ColorButton color="oliveGreen" borderColor="#65875A" bgColor="#65875A" />
-                            </View>
-                        </View>
-
-                        <View style={{ flex: 1 }}>
-                            <FormField
-                                value={name}
-                                handleChangeText={handleNameChange}
-                                title={'Name'}
-                                placeholder={'Enter name for this type'}   
-                                otherStyles={'mb-5'}
-                            />
-                            <FormField
-                                value={icon}
-                                maxLength={2}
-                                handleChangeText={handleIconChange}
-                                title={'Icon'}
-                                placeholder={'Enter emoji for this type'}   
-                                otherStyles={'mb-5'}
-                                error={getIconError()}
-                                errorText={getIconError()}
-                            />
-                        </View>
-                    </View>
-                </ScrollView>
-
-                <View style={{ padding: 16 }}>
-                    <TouchableOpacity
-                        onPress={handleSaveNoteType} 
-                        className="bg-black justify-center rounded-[34px] h-[56px] items-center w-full"
-                    >
-                        <Text className="text-white font-cygrebold text-[18px] text-center">
-                            Save
-                        </Text>
-                    </TouchableOpacity>
-                </View>
-            </KeyboardAvoidingView>
-        </BookBottomDrawer>
-    );
-}
---
+++ components/NoteTypeDrawer.jsx
@@ -0,0 +1,56 @@
+import BookBottomDrawer from "./BottomDrawer";
+import NoteType from "./NoteType";
+import { Text, FlatList, TouchableOpacity } from 'react-native';
+
+
+const NoteTypeDrawer = ({ 
+    noteTypes,
+    isNoteDrawerOpen,
+    setIsNoteDrawerOpen,
+    setIsNoteTypeDrawerOpen,
+    setNoteTypesSelected,
+    noteTypesSelected
+ }) => {
+
+
+    const handleChoseNoteType = (id) => {
+        //update single entry, the rest set to false
+        setNoteTypesSelected((prev) => {
+        const resetObj = Object.fromEntries(Object.keys(prev).map(key => [key, false]));
+            return {...resetObj, [id]: !prev[id]};
+        });
+    }
+
+    return (
+            <BookBottomDrawer
+                height="55%"
+                isBottomSheetOpen={isNoteDrawerOpen}
+                setIsBottomSheetOpen={setIsNoteDrawerOpen}
+                containerStyles={'pb-0 flex-1'}
+            >
+                    <Text className="font-cygrebold text-[22px] mt-9 leading-[26.4px] text-center mb-5">Choose Note’s Type</Text>
+                    <FlatList
+                        className="w-full mx-4 flex-1"
+                        data={noteTypes}
+                        ListEmptyComponent={() => <Text className="text-[20px] font-cygrebold">No Note Types Yet</Text>}
+                        renderItem={({ item }) =>
+                            <NoteType
+                                key={item.id}
+                                name={item.name}
+                                icon={item.icon}
+                                bgColor={item.bgColor}
+                                selected={noteTypesSelected[item.id]}
+                                onPress={() => handleChoseNoteType(item.id)}
+                            />}
+                    />
+                    <TouchableOpacity
+                        onPress={() => setIsNoteTypeDrawerOpen(true)}
+                        className="bg-black mb-2 justify-center rounded-[34px] max-h-[56px] items-center w-full h-full">
+                        <Text className="text-white font-cygrebold text-[18px] text-center">Add New Type</Text>
+                    </TouchableOpacity>
+            </BookBottomDrawer>
+        );
+}
+
+
+export default NoteTypeDrawer;
