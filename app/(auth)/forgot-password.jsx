00db891cd9fddd4c32b130c81358970e2dcf024f
Tomas <bekjon_98@mail.ru>
2025-01-10 20:29:24 +0200

    added field validation for forgot-password screen

--- app/(auth)/forgot-password.jsx
+++ app/(auth)/forgot-password.jsx
@@ -1,30 +1,58 @@
 import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import {  router } from 'expo-router';
 import FormField from '../../components/FormField';
 import Stage from '../../components/Stage';
 import { UserContext } from '../../context/UserContext';
 import axios from '../../network/axios';
+import { HttpStatusCode } from 'axios';
 
 const ForgotPassword = () => {
   
   const { recoveryEmail, setRecoveryEmail } = useContext(UserContext);
 
+  const [errors, setErrors] = useState({ email: '' });
+
+  const hasEmailError = () => errors['email'];
+
+  const hasErrors = () => {
+    return !recoveryEmail;
+  }
+
+  const validateAll = () => {
+    if (!recoveryEmail) {
+      setErrors(prev => ({...prev, email: 'Email cannot be empty'}))
+    }
+  }
+
+  const clearAllErrors = () => {
+    setErrors(prev => ({...prev, email: ''}));
+  }
+
   const sendEmail = async () => {
     try {
+      clearAllErrors();
+      validateAll();
+      if (hasErrors()) return;
       await axios.post('/forgot-password', { email: recoveryEmail });
       router.push('/verification')
     }
     catch(error) {
       console.log(error);
+      handleError(error);
+    }
+  }
+
+  const handleError = (error) => {
+    if (error?.response?.status === HttpStatusCode.NotFound) {
+      setErrors(prev => ({...prev, email: 'Email not found'}));
     }
   }
 
   const handleSubmit = async () => {
     await sendEmail();
   }
-
 
   return (
     <SafeAreaView className="bg-[#F7F7F7] h-full">
@@ -49,6 +77,8 @@ const ForgotPassword = () => {
                   otherStyles={'max-h-[52px] h-full mb-[50px]'}
                   handleChangeText={(e) => setRecoveryEmail(e) }
                   value={recoveryEmail}
+                  error={hasEmailError()}
+                  errorText={hasEmailError()}
                 />
                 <TouchableOpacity
                   onPress={handleSubmit}
