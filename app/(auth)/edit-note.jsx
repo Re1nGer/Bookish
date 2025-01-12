3b209efdd02c7cfcda4f26f98519c14d57815c9a
Tomas <bekjon_98@mail.ru>
2025-01-12 17:46:33 +0200

    updated edit-note screen, now it fetches note collections too

--- app/(auth)/edit-note.jsx
+++ app/(auth)/edit-note.jsx
@@ -53,6 +53,10 @@ const EditNote = () => {
         setSelection(event.nativeEvent.selection);
     };
 
+    const handleRemoveNoteCollection = (collectionId) => {
+        setNote(prev => ({...prev, collections: prev.collections.filter(item => item.id !== collectionId)})) ;
+    }
+
     const getSelectedNoteTypeId = () => {
         return Object.keys(noteTypesSelected).find(id => noteTypesSelected[id] === true);
     }
@@ -87,13 +91,18 @@ const EditNote = () => {
         return note?.quote?.id ?? null;
     }
 
+    const getSelectedCollectionIds = () => {
+        return note.collections.map(item => item.id);
+    }
+
     const handleSaveNote = async () => {
         try {
             await axios.put(`books/${bookId}/note`, {
                 id: noteId,
                 content: text,
                 typeId: getSelectedNoteTypeId(),
-                quoteId: getSelectedQuoteId()
+                quoteId: getSelectedQuoteId(),
+                collectionIds: getSelectedCollectionIds()
                 //TODO: to add collections ids, quoteId, repetition groups id
             });
             router.back();
@@ -129,7 +138,10 @@ const EditNote = () => {
                 book: data?.quote?.bookName,
                 text: data?.quote?.text
             }
-            setNote(prev => ({...prev, quote: data.quote ? relatedQuote: null}))
+            setNote(prev => ({...prev,
+                quote: data.quote ? relatedQuote: null,
+                collections: data.collections
+            }))
             return data;
 
         } catch(error) {
@@ -254,10 +266,18 @@ const EditNote = () => {
                     <Text className="text-black text-[22px] leading-[26.4px] font-cygrebold mb-2.5">Collections</Text>
                     <View className="flex-wrap p-5 border bg-black max-h-[126px] h-full flex-row items-center rounded-[20px]">
                         <View className="flex-wrap flex-row justify-start self-start flex-1">
-                            <Genre name={'For psychology classes'} showCloseBtn containerStyles={'max-w-[200px]'} />
+                            { note?.collections?.map(item =>
+                                <Collection
+                                    key={item.id}
+                                    id={item.id}
+                                    name={item.name}
+                                    showCloseBtn={true}
+                                    handleRemove={handleRemoveNoteCollection}
+                                  />
+                                ) }
                         </View>
                         <TouchableOpacity
-                            onPress={() => router.push('select-collections')}
+                            onPress={() => router.push('select-note-collections')}
                             className="items-center flex-1 self-center bg-[#fff] max-w-[61px] max-h-[62px] rounded-full justify-center p-4">
                             <MaterialIcons name="add" size={30} />
                         </TouchableOpacity>
@@ -290,6 +310,20 @@ const EditNote = () => {
             <View className="h-[50px]"></View>
             </ScrollView>
     </SafeAreaView>
+}
+
+const Collection = ({ id, name, showCloseBtn, handleRemove, containerStyles }) => {
+
+    return <View className={`py-2 px-1 mr-2 mb-2 max-w-[116px] bg-primary flex-row items-center justify-between rounded-[5px] ${containerStyles}`}>
+        <Text className="text-[#FFFFFF] font-cygrebold leading-[16.8px] text-sm px-1" numberOfLines={1} ellipsizeMode='tail'>{name}</Text>
+        { showCloseBtn ? (
+            <TouchableOpacity
+                onPress={() => handleRemove(id)}
+                className="rounded-full bg-[#fff] items-center ml-1 mr-2.5 justify-center w-[16px] h-[16px]">
+                <MaterialIcons name='close' color={'#6592E3'} size={8} />
+            </TouchableOpacity>
+        ) : <></> }
+    </View>
 }
 
 
