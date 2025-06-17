2ce239220fb839e037732b3f04b7fe37f11960bf
Tomas <bekjon_98@mail.ru>
2025-06-17 23:16:35 +0300

    added book calendar component

--- app/(auth)/book-calendar.jsx
+++ app/(auth)/book-calendar.jsx
@@ -1,18 +1,19 @@
 import React, {useState, Fragment, useCallback, useMemo, useRef} from 'react';
-import {StyleSheet, View, ScrollView, Text, TouchableOpacity, Image} from 'react-native';
-import {Calendar, CalendarUtils, LocaleConfig} from 'react-native-calendars';
+import { StyleSheet, View, ScrollView, Text, TouchableOpacity, Image } from 'react-native';
+import { router } from 'expo-router';
+import { Calendar, CalendarUtils } from 'react-native-calendars';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { MaterialIcons } from '@expo/vector-icons';
 import { images } from '../../constants';
 
 const INITIAL_DATE = '2025-04-08';
 
-const today = new Date().getDay()
 
-
-const DayComponent = ({ date, state, marking, onPress }) => {
+const DayComponent = ({ date, state, marking, onPress, ...rest }) => {
     //console.log(props)
 
     const isSelected = state === 'today';
+    console.log(marking)
     //array of read books at the day
 
     return (
@@ -22,42 +23,27 @@ const DayComponent = ({ date, state, marking, onPress }) => {
             {date.day}
           </Text>
         </View>
-{/*         <Image
-          className="max-w-[37px] max-h-[53px]"
-          height={53}
-          width={37}
-          source={images.book1}
-          resizeMode='cover'
-        /> */}
+        { marking && (
+          <Image
+            className={`max-w-[37px] ${isSelected ? 'mt-1' : ''} max-h-[53px]`}
+            height={53}
+            width={37}
+            source={images.book1}
+            resizeMode='cover'
+          />
+        ) }
       </TouchableOpacity>
-    )
-}
-
-const Header = (props) => {
-    console.log(props)
-    return (
-      <View className="absolute -top-4">
-        <Text>
-          {"some content"}
-        </Text>
-      </View>
     )
 }
 
 
 const BookCalendar = () => {
+
     const [selected, setSelected] = useState(INITIAL_DATE);
-    const [markedDates, setMarkedDates] = useState({});
 
-    const getDate = (count) => {
-        const date = new Date(INITIAL_DATE);
-        const newDate = date.setDate(date.getDate() + count);
-        return CalendarUtils.getCalendarDateString(newDate);
-    };
-
-    const onDayPress = useCallback((day) => {
-        setSelected(day.dateString);
-    }, []);
+    const [markedDates, setMarkedDates] = useState({
+      '2025-06-06': {selected: true, marked: true, selectedColor: 'blue'},
+    });
 
     const handleDayPress = (day) => {
       setSelected(day.dateString);
@@ -72,33 +58,18 @@ const BookCalendar = () => {
     const renderArrow = (direction) => {
       if (direction === 'left') {
         return (
-          <View className="bg-primary items-center justify-center w-[42px] h-[43px] rounded-[12px]">
+          <View className="bg-primary ml-[-15px] items-center justify-center w-[42px] h-[43px] rounded-[12px]">
             <Text className="text-white font-cygrebold font-bold text-lg">{'<'}</Text>
           </View>
         );
       } 
 
       return (
-        <View className="bg-primary items-center justify-center w-[42px] h-[43px] rounded-[12px]">
+        <View className="bg-primary mr-[-15px] items-center justify-center w-[42px] h-[43px] rounded-[12px]">
           <Text className="text-white font-cygrebold font-bold text-lg">{'>'}</Text>
         </View>
       );
     };
-
-/*     const marked = useMemo(() => {
-        return {
-        [getDate(-1)]: {
-            dotColor: 'red',
-            marked: true
-        },
-        [selected]: {
-            selected: true,
-            disableTouchEvent: true,
-            selectedColor: 'orange',
-            selectedTextColor: 'red'
-        }
-        };
-    }, [selected]); */
 
     const theme = {
       dayNamesShort: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
@@ -110,13 +81,10 @@ const BookCalendar = () => {
             <Calendar
               theme={theme}
                 enableSwipeMonths
+                headerStyle={styles.headerStyle}
                 renderArrow={renderArrow}
-                  onPressArrowLeft={subtractMonth => {
-                    console.log('fire')
-                    return subtractMonth()
-                  }}
-                  onPressArrowRight={addMonth => addMonth()}
-                //current={INITIAL_DATE}
+                onPressArrowLeft={subtractMonth => subtractMonth()}
+                onPressArrowRight={addMonth => addMonth()}
                 style={styles.calendar}
                 onDayPress={handleDayPress}
                 markedDates={markedDates}
@@ -128,8 +96,21 @@ const BookCalendar = () => {
 
 
     return <SafeAreaView className="flex-1 h-full bg-[#F7F7F7]">
-      <View className="justify-center h-full">
-        <View className="mx-5 border rounded-[10px] max-h-[800px] border-[#8A8A8A]">
+        <View className="max-h-[40px] justify-between items-center flex-row h-full mx-5 mb-7">
+            <View className="flex-row flex-1">
+            <TouchableOpacity
+                className="flex-1" onPress={() => router.back()}>
+                <Image source={require('../../assets/images/left_arrow.png')} />
+            </TouchableOpacity>
+            </View>
+        </View>
+
+      <View className="justify-start h-full">
+        <View className="mx-5 mb-7 flex-[.5] justify-start">
+          <Text className="text-black font-cygrebold text-[28px]">Book Calendar</Text>
+          <Text className="text-black font-cygresemibold max-w-[75%]">See your finished books and captured emotions when finishing the book here.</Text>
+        </View>
+        <View className="mx-5 rounded-[10px] justify-center max-h-[2000px]">
             {renderCalendarWithSelectableDate()}
         </View>
       </View>
@@ -139,21 +120,26 @@ const BookCalendar = () => {
 const styles = StyleSheet.create({
   calendar: {
     borderRadius: 20,
-    //height: '80%'
+    borderColor: '#8A8A8A',
+    borderWidth: 1
+  },
+  headerStyle: {
+    position: 'absolute',
+    top: -100,
+    backgroundColor: 'transparent',
+    shadowColor: 'transparent',
+    tintColor: "transparent",
+    width: "100%",
+    paddingHorizontal: -100,
+
   },
   switchContainer: {
     flexDirection: 'row',
-    margin: 10,
     alignItems: 'center'
-  },
-  switchText: {
-    margin: 10,
-    fontSize: 16
   },
   text: {
     textAlign: 'center',
     padding: 10,
-    backgroundColor: 'lightgrey',
     fontSize: 16
   },
   disabledText: {
@@ -162,30 +148,8 @@ const styles = StyleSheet.create({
   defaultText: {
     color: 'purple'
   },
-  customCalendar: {
-    height: 150,
-    borderBottomWidth: 1,
-    borderBottomColor: 'lightgrey'
-  },
   customDay: {
     textAlign: 'center'
-  },
-  customHeader: {
-    backgroundColor: '#FCC',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    marginHorizontal: -4,
-    padding: 8
-  },
-  customTitleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: 10
-  },
-  customTitle: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: '#00BBF2'
   }
 });
 
